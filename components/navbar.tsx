20cc2af7bb38014f46f8ff7159067f1ea2465f54
sakkurthi sashank <sakkurthisashank@gmail.com>
2022-10-02 09:33:22 +0530

    fix: auth

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,8 +3,8 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { MdOutlineNotifications } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 import { BiSearch } from "react-icons/bi";
-import {Sidebar} from "./sidebar";
-
+import { Sidebar } from "./sidebar";
+import { signIn, useSession } from "next-auth/react";
 
 export const Navbar: React.FC<{}> = () => {
   const [openSideBar, setOpenSideBar] = React.useState(false);
@@ -65,6 +65,7 @@ const SearchBox: React.FC<{}> = () => {
 
 const Profile: React.FC<{}> = () => {
   const [isProfileOpen, setIsProfileOpen] = React.useState(false);
+  const { data: sessions } = useSession();
   return (
     <div className="flex items-center justify-end space-x-4 md:w-1/3">
       <button className="relative">
@@ -79,15 +80,25 @@ const Profile: React.FC<{}> = () => {
           0
         </div>
       </button>
-      <button onClick={() => setIsProfileOpen(!isProfileOpen)}>
-        <picture>
-          <img
-            className="h-8 w-8 rounded-full"
-            src="https://bit.ly/dan-abramov"
-            alt=""
-          />
-        </picture>
-      </button>
+      {sessions ? (
+        <button onClick={() => setIsProfileOpen(!isProfileOpen)}>
+          <picture>
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://bit.ly/dan-abramov"
+              alt=""
+            />
+          </picture>
+        </button>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="rounded-md bg-sky-400 px-2 py-1 font-inter text-white hover:bg-sky-500"
+        >
+          Sign In
+        </button>
+      )}
+
       {isProfileOpen ? (
         <div className="absolute top-[58px] right-1 z-10 flex h-48 w-40 flex-col space-y-1 rounded-md border border-gray-300 bg-white p-2">
           <button className="rounded-md py-1 font-inter text-gray-800 hover:bg-pink-500 hover:text-white">
